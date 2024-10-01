868a45e0506a8b8b3fc1ce8b93d04be3e4cc9ac5
amelialsylvest <amelialsylvest@gmail.com>
2024-10-01 12:08:19 +0000

    general fix

---
+++ .idx/dev.nix
@@ -0,0 +1,54 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+    pkgs.python3
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+      "aaron-bond.better-comments"
+      "Catppuccin.catppuccin-vsc"
+      "christian-kohler.path-intellisense"
+      "circlecodesolution.ccs-flutter-color"
+      "esbenp.prettier-vscode"
+      "formulahendry.auto-rename-tag"
+      "glenn2223.live-sass"
+      "oderwat.indent-rainbow"
+      "PKief.material-icon-theme"
+      "ritwickdey.LiveServer"
+    ];
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["python3" "-m" "http.server" "$PORT" "--bind" "0.0.0.0"];
+          manager = "web";
+        };
+      };
+    };
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [ "style.css" "index.html" ];
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
